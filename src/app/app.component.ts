7670452e1a6b5ac437c579dde2060a27d174de37
RenanBilhan <renan.bilhan@hotmail.com>
2025-03-07 08:13:03 -0300

    feat: Swiper adicionado mas ainda nao estilizado.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,8 @@ import { CommonModule } from '@angular/common';
   selector: 'app-root',
   imports: [RouterOutlet, HomeComponent, HeaderComponent , CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  
 })
 export class AppComponent {
   title = 'meu-primeiro-app';
--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,17 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-slider',
   imports: [ CommonModule ],
   templateUrl: './slider.component.html',
-  styleUrl: './slider.component.css'
+  styleUrl: './slider.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SliderComponent {  
 
   myImages = [
-    {src: '/1.jpeg'},
-    {src: "/2.jpeg"},
+    {src: '/1.jpeg', id: 'img1'},
+    {src: "/2.jpeg", id: 'img2'},
     // {src: "src\assets\imagens\fotos\WhatsApp Image 2025-02-27 at 08.08.10.jpeg"},
     // {src: "src\assets\imagens\fotos\WhatsApp Image 2025-02-27 at 08.08.11 (1).jpeg"},
     // {src: "src\assets\imagens\fotos\WhatsApp Image 2025-02-27 at 08.08.11.jpeg"}
